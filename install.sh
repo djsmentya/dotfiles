1e8d11d0d2e1b15aac2f4d20799e5bfac22bf51b
Eugene Smentyna <djsmentya@gmail.com>
2016-09-06 14:24:15 +0000

    Add rvm, janus, zsh

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
+sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
 sudo apt-get update
-sudo apt-get install -y zsh
+sudo apt-get install -y zsh git vim ctags curl rcm
 
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# Install rvm
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
+source /etc/profile.d/rvm.sh
+
+# Install vim janus
+curl -L https://bit.ly/janus-bootstrap | bash
+
+# Instal vim oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
