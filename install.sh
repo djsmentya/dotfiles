8d0179fd8341d2672ddf20e74fbabe74d6eb68ed
Eugene Smentyna <djsmentya@gmail.com>
2016-09-07 08:36:25 +0000

    source rvm after install

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y zsh git vim exuberant-ctags curl rcm
 # Install rvm
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://raw.githubusercontent.com/wayneeseguin/rvm/master/binscripts/rvm-installer | bash -s stable --ruby
-source /etc/profile.d/rvm.sh
+source ~/.rvm/scripts/rvm
 
 # Install vim janus
 curl -L https://bit.ly/janus-bootstrap | bash
